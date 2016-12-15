0ed83236ae21c5c30a203933ed0662e169e747b4
Douglas Wittnebert <dougwit@gmail.com>
2016-12-15 14:03:57 -0500

    onClickListener test for chess pieces

--- app/src/main/java/website/kale/androidchess/PlayGameActivity.java
+++ app/src/main/java/website/kale/androidchess/PlayGameActivity.java
@@ -16,7 +16,9 @@ import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
+import android.widget.Toast;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 public class PlayGameActivity extends AppCompatActivity {
@@ -25,6 +27,33 @@ public class PlayGameActivity extends AppCompatActivity {
     List<String> chessPieces;
     ImageView origin;
     ChessGame chessGame;
+    String moveString = "";
+    boolean needsPiece = true;
+
+    class ChessPieceListener implements View.OnClickListener {
+        private final int row, column;
+
+        public ChessPieceListener(int row, int column) {
+            this.row = row;
+            this.column = column;
+        }
+
+        @Override
+        public void onClick(View view) {
+            ImageView thisView = (ImageView) view;
+            if (origin == null) {
+                thisView.setBackgroundColor(Color.RED);
+                origin = thisView;
+                moveString += ChessBoard.columnToChessFile(column) + ChessBoard.rowToChessRank(row);
+            } else {
+                moveString += " " + ChessBoard.columnToChessFile(column) + ChessBoard.rowToChessRank(row);
+                Toast.makeText(PlayGameActivity.this, moveString, Toast.LENGTH_SHORT).show();
+                moveString = "";
+                origin.setBackgroundColor(0);
+                origin = null;
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +67,8 @@ public class PlayGameActivity extends AppCompatActivity {
     }
 
     private void drawBoard() {
-        for (int c = 0; c < 8; c++) {
-            for (int r = 0; r < 8; r ++) {
+        for (int r = 0; r < 8; r++) {
+            for (int c = 0; c < 8; c++) {
                 ImageView newSpace = new ImageView(this);
                 GridLayout.LayoutParams param =new GridLayout.LayoutParams();
                 param.height = 88;
@@ -61,10 +90,11 @@ public class PlayGameActivity extends AppCompatActivity {
     }
 
     private void drawPieces() {
-        for (int c = 0; c < 8; c++) {
-            for (int r = 0; r < 8; r++) {
+        for (int r = 0; r < 8; r++) {
+            for (int c = 0; c < 8; c++) {
                 ImageView newSpace = new ImageView(this);
-                GridLayout.LayoutParams param =new GridLayout.LayoutParams();
+                String imageResourceString = null;
+                GridLayout.LayoutParams param = new GridLayout.LayoutParams();
                 param.height = 88;
                 param.width = 88;
                 param.topMargin = 0;
@@ -72,22 +102,81 @@ public class PlayGameActivity extends AppCompatActivity {
                 param.columnSpec = GridLayout.spec(c);
                 param.rowSpec = GridLayout.spec(r);
                 newSpace.setLayoutParams(param);
-                newSpace.setImageResource(R.drawable.white_pawn);
-                newSpace.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        ImageView thisView = (ImageView) view;
-                        if (origin == null) {
-                            thisView.setBackgroundColor(Color.RED);
-                            origin = thisView;
-                        } else {
-                            origin.setBackgroundColor(0);
-                            origin = null;
-                        }
+
+                imageResourceString = buildPieceImageStringByCoordinates(r, c);
+                if (imageResourceString != null) {
+                    try {
+                        Class res = R.drawable.class;
+                        Field field = res.getField(imageResourceString);
+                        int drawableId = field.getInt(null);
+                        newSpace.setImageResource(drawableId);
                     }
-                });
+                    catch (Exception e) {
+                    }
+                }
+
+                newSpace.setOnClickListener(new ChessPieceListener(r, c));
                 chessGrid.addView(newSpace);
             }
         }
+    }
+
+    private String buildPieceImageStringByCoordinates(int row, int column) {
+        if (needsPiece) {
+            needsPiece = false;
+            return "black_bishop";
+        } else {
+            needsPiece = true;
+            return null;
+        }
+
+
+//        Square coordinates = new Square(row, column);
+//        ChessPiece piece = chessGame.pieceAt(coordinates);
+//        String imageIdString = null;
+//
+//        switch (piece.getColor()) {
+//            case BLACK:
+//                imageIdString += "black_";
+//                break;
+//
+//            case WHITE:
+//                imageIdString += "white_";
+//                break;
+//
+//            default:
+//                return null;
+//        }
+//
+//        switch (piece.getType()) {
+//            case PAWN:
+//                imageIdString += "pawn";
+//                break;
+//
+//            case BISHOP:
+//                imageIdString += "bishop";
+//                break;
+//
+//            case KNIGHT:
+//                imageIdString += "knight";
+//                break;
+//
+//            case ROOK:
+//                imageIdString += "rook";
+//                break;
+//
+//            case QUEEN:
+//                imageIdString += "queen";
+//                break;
+//
+//            case KING:
+//                imageIdString += "king";
+//                break;
+//
+//            default:
+//                return null;
+//        }
+//
+//        return imageIdString;
     }
 }
