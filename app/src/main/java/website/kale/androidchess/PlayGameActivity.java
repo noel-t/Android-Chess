78568066aad53ca5b1db7f9d66c6fa6f1a26f75e
Douglas Wittnebert <dougwit@gmail.com>
2016-12-15 00:39:31 -0500

    onClickListener test for chess pieces

--- app/src/main/java/website/kale/androidchess/PlayGameActivity.java
+++ app/src/main/java/website/kale/androidchess/PlayGameActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Button;
@@ -22,13 +23,17 @@ public class PlayGameActivity extends AppCompatActivity {
     GridLayout chessGrid;
     ArrayAdapter chessGridAdapter;
     List<String> chessPieces;
+    ImageView origin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        origin = null;
         setContentView(R.layout.activity_play_game);
         chessGrid = (GridLayout) findViewById(R.id.chessboard);
+
         drawBoard();
+        drawPieces();
     }
 
     private void drawBoard() {
@@ -36,15 +41,13 @@ public class PlayGameActivity extends AppCompatActivity {
             for (int r = 0; r < 8; r ++) {
                 ImageView newSpace = new ImageView(this);
                 GridLayout.LayoutParams param =new GridLayout.LayoutParams();
-                param.height = LayoutParams.MATCH_PARENT;
-                param.width = LayoutParams.MATCH_PARENT;
-                param.rightMargin = 0;
+                param.height = 88;
+                param.width = 88;
                 param.topMargin = 0;
                 param.setGravity(Gravity.FILL_HORIZONTAL);
                 param.columnSpec = GridLayout.spec(c);
                 param.rowSpec = GridLayout.spec(r);
-                
-                newSpace.setLayoutParams (param);
+                newSpace.setLayoutParams(param);
                 if ((r + c) % 2 == 0)
                     newSpace.setImageResource(R.drawable.light_space);
                 else
@@ -54,5 +57,37 @@ public class PlayGameActivity extends AppCompatActivity {
             }
         }
 
+    }
+
+    private void drawPieces() {
+        for (int c = 0; c < 8; c++) {
+            for (int r = 0; r < 8; r++) {
+                ImageView newSpace = new ImageView(this);
+                GridLayout.LayoutParams param =new GridLayout.LayoutParams();
+                param.height = 88;
+                param.width = 88;
+                param.topMargin = 0;
+                param.setGravity(Gravity.FILL_HORIZONTAL);
+                param.columnSpec = GridLayout.spec(c);
+                param.rowSpec = GridLayout.spec(r);
+                newSpace.setLayoutParams(param);
+                newSpace.setImageResource(R.drawable.white_pawn);
+                newSpace.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        ImageView thisView = (ImageView) view;
+                        if (origin == null) {
+                            thisView.setBackgroundColor(Color.RED);
+                            origin = thisView;
+
+                        } else {
+                            origin.setBackgroundColor(0);
+                            origin = null;
+                        }
+                    }
+                });
+                chessGrid.addView(newSpace);
+            }
+        }
     }
 }
