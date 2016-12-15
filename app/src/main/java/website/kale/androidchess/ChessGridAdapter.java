a00e86fecd6a899de71e901b72a72786e0f8c405
Douglas Wittnebert <dougwit@gmail.com>
2016-12-14 23:50:29 -0500

    okay

--- app/src/main/java/website/kale/androidchess/ChessGridAdapter.java
+++ app/src/main/java/website/kale/androidchess/ChessGridAdapter.java
@@ -1,33 +1,23 @@
 package website.kale.androidchess;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+
+import java.util.List;
 
 /**
  * Created by douglasw on 12/14/16.
  */
 
-public class ChessGridAdapter extends BaseAdapter {
+public class ChessGridAdapter extends ArrayAdapter<ChessPiece> {
     ChessPiece[] pieces;
 
-    @Override
-    public int getCount() {
-        return 0;
+    public ChessGridAdapter(Context context, int resource, List<ChessPiece> objects) {
+        super(context, resource, objects);
     }
 
-    @Override
-    public Object getItem(int i) {
-        return null;
-    }
-
-    @Override
-    public long getItemId(int i) {
-        return 0;
-    }
-
-    @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
-    }
+    
 }
--- app/src/main/java/website/kale/androidchess/PlayGameActivity.java
+++ app/src/main/java/website/kale/androidchess/PlayGameActivity.java
@@ -2,6 +2,7 @@ package website.kale.androidchess;
 
 import android.app.ActionBar;
 import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -12,6 +13,7 @@ import android.widget.GridLayout;
 import android.widget.GridLayout.LayoutParams;
 import android.widget.GridView;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListAdapter;
 
 import java.util.List;
@@ -25,31 +27,32 @@ public class PlayGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_game);
-        chessGrid = (GridLayout) findViewById(R.id.activity_play_game);
-//        drawBoard();
+        chessGrid = (GridLayout) findViewById(R.id.chessboard);
+        drawBoard();
     }
 
-//    private void drawBoard() {
-//        for (int c = 0; c < 8; c++) {
-//            for (int r = 0; r < 8; r ++) {
-//                Button newSpace = new Button(this);
-//                GridLayout.LayoutParams param =new GridLayout.LayoutParams();
-//                param.height = LayoutParams.WRAP_CONTENT;
-//                param.width = LayoutParams.WRAP_CONTENT;
-//                param.rightMargin = 0;
-//                param.topMargin = 0;
-//                param.setGravity(Gravity.CENTER);
-//                param.columnSpec = GridLayout.spec(c);
-//                param.rowSpec = GridLayout.spec(r);
-//                newSpace.setLayoutParams (param);
-//                if ((r + c) % 2 == 0)
-//                    newSpace.setBackgroundColor(getResources().getColor(R.color.chessTan));
-//                else
-//                    newSpace.setBackgroundColor(getResources().getColor(R.color.chessBrown));
-//
-//                chessGrid.addView(newSpace);
-//            }
-//        }
-//
-//    }
+    private void drawBoard() {
+        for (int c = 0; c < 8; c++) {
+            for (int r = 0; r < 8; r ++) {
+                ImageView newSpace = new ImageView(this);
+                GridLayout.LayoutParams param =new GridLayout.LayoutParams();
+                param.height = LayoutParams.MATCH_PARENT;
+                param.width = LayoutParams.MATCH_PARENT;
+                param.rightMargin = 0;
+                param.topMargin = 0;
+                param.setGravity(Gravity.FILL_HORIZONTAL);
+                param.columnSpec = GridLayout.spec(c);
+                param.rowSpec = GridLayout.spec(r);
+                
+                newSpace.setLayoutParams (param);
+                if ((r + c) % 2 == 0)
+                    newSpace.setImageResource(R.drawable.light_space);
+                else
+                    newSpace.setImageResource(R.drawable.dark_space);
+
+                chessGrid.addView(newSpace);
+            }
+        }
+
+    }
 }
